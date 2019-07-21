49c7cb45ca2cd7e947094f7a1b4fc034ebf4b9df
Ademola Akingbade <ade_aking14@yahoo.com>
2019-07-21 10:52:34 +0100

    ADDED logic to inforce strong password using class validators and pipes

--- src/tasks/dto/create-task.dto.ts
+++ src/tasks/dto/create-task.dto.ts
@@ -1,6 +1,9 @@
 import { IsNotEmpty } from 'class-validator';
 
 export class CreateTaskDto {
+  // Class validator which at runtime do magic to the class
+  // This can then be used by validation pipes to validate the data
+  // against these rules
   @IsNotEmpty()
   title: string;
   @IsNotEmpty()
