9fac60de442c3f2c80d1ed0c296cd80f49a36a34
Ademola Akingbade <ade_aking14@yahoo.com>
2019-07-21 10:38:23 +0100

    ADDED new signup endpoint including logic to save user in db

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,7 +1,16 @@
 import { Repository, Entity, EntityRepository } from 'typeorm';
 import { User } from './user.entity';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
+  async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    const { username, password } = authCredentialsDto;
 
+    const user = new User();
+    user.username = username;
+    user.password = password;
+    // This will just return a 201 created if left alone
+    await user.save();
+  }
 }
