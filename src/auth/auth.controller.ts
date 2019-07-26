580ebdb439672d4358cef1dc4f8768266988e188
Ademola Akingbade <ade_aking14@yahoo.com>
2019-07-26 11:56:47 +0100

    ADDED custom decorator to get user from request object

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,6 +2,8 @@ import { Controller, Post, Body, ValidationPipe, UseGuards, Req } from '@nestjs/
 import { AuthService } from './auth.service';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { AuthGuard } from '@nestjs/passport';
+import { GetUser } from './decorators/get-user.decorator';
+import { User } from './user.entity';
 
 @Controller('auth')
 export class AuthController {
@@ -19,7 +21,7 @@ export class AuthController {
 
   @Post('/test')
   @UseGuards(AuthGuard()) // Request level authentication guard - It will validate the JWT in the request
-  test(@Req() req) {
-    console.log(req);
+  test(@GetUser() user: User) {
+    console.log(user);
   }
 }
