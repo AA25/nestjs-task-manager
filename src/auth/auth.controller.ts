c1d9139eb316c20b063939233a6871393960d680
Ademola Akingbade <ade_aking14@yahoo.com>
2019-07-26 12:04:55 +0100

    REMOVED redundant endpoint

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,9 +1,6 @@
 import { Controller, Post, Body, ValidationPipe, UseGuards, Req } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
-import { AuthGuard } from '@nestjs/passport';
-import { GetUser } from './decorators/get-user.decorator';
-import { User } from './user.entity';
 
 @Controller('auth')
 export class AuthController {
@@ -17,11 +14,5 @@ export class AuthController {
   @Post('/signin')
   signIn(@Body(ValidationPipe) authCredentialsDto: AuthCredentialsDto): Promise<{ jwtToken: string }> {
     return this.authService.signIn(authCredentialsDto);
-  }
-
-  @Post('/test')
-  @UseGuards(AuthGuard()) // Request level authentication guard - It will validate the JWT in the request
-  test(@GetUser() user: User) {
-    console.log(user);
   }
 }
