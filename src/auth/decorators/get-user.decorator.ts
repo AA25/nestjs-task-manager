8abc56398b8663cc4b589c4689a17380ffa13295
Ademola Akingbade <ade_aking14@yahoo.com>
2019-08-18 19:01:01 +0100

    ADDED comments for custom decorator

--- src/auth/decorators/get-user.decorator.ts
+++ src/auth/decorators/get-user.decorator.ts
@@ -1,6 +1,9 @@
 import { createParamDecorator } from '@nestjs/common';
 import { User } from '../user.entity';
 
+// Creating a custom @decorator
+// Assuming the request has been authenticated by passport
+// this decorator will return the user from the request object
 export const GetUser = createParamDecorator((data, req): User => {
   return req.user;
 });
