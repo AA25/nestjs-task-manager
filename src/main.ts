ffd63e3efe3736cbf2297f3a10821da711ca7e0c
Ademola Akingbade <ade_aking14@yahoo.com>
2019-08-19 19:30:51 +0100

    UPDATED project to use config from config files

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,17 @@
 import { NestFactory } from '@nestjs/core';
 import { Logger } from '@nestjs/common';
 import { AppModule } from './app.module';
+import * as config from 'config';
 
 async function bootstrap() {
+  // Since the env var NODE_ENV is not defined
+  // development will be used as the environment
+  const serverConfig = config.get('server');
   const logger = new Logger('bootstrap');
   // The root module is the starting point of the application
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  await app.listen(serverConfig.port);
   logger.log(`Application listening on port`);
 }
 bootstrap();
