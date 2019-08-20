a56f070cb68be69a36e015762ddf485ef2ee0878
Ademola Akingbade <ade_aking14@yahoo.com>
2019-08-20 18:51:56 +0100

    UPDATED app to enable CORS

--- src/main.ts
+++ src/main.ts
@@ -5,16 +5,23 @@ import { AppModule } from './app.module';
 import * as config from 'config';
 
 async function bootstrap() {
+  // The root module is the starting point of the application
+  const app = await NestFactory.create(AppModule);
+  const logger = new Logger('bootstrap');
+
   // Since the env var NODE_ENV is not defined
   // development will be used as the environment
   const serverConfig = config.get('server');
-  const logger = new Logger('bootstrap');
-  // The root module is the starting point of the application
-  const app = await NestFactory.create(AppModule);
 
   // Assume env port variable before config port
   // incase config port needs to be overriding for some reason
   const port = process.env.PORT || serverConfig.port;
+
+  // TODO:
+  // if (process.env.NODE_ENV === 'development') {
+  //   app.enableCors();
+  // }
+  app.enableCors();
   await app.listen(port);
   logger.log(`Application listening on port`);
 }
